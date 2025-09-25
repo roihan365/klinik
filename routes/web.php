79ca6fedc011834a765840f6c3b2b93e30282ca9
NurRizkaZahra <nz482524@gmail.com>
2025-09-25 10:07:12 +0800

    dashboard-pasien
    B
    
    rename dashboard.blade.php to dashboard-pasien.blade.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dashboard-pasien', function () {
+    return view('dashboard-pasien');
+})->middleware(['auth', 'verified'])->name('dashboard-pasien');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
